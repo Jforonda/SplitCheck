758d433bf3cbcf7f8c406a1199e05be0a0d5d0a6
james.foronda <strafious@gmail.com>
2017-07-29 22:18:06 -0700

    Added Google Services

--- splitcheckmathlibrary/src/main/java/com/android/SplitCheckMath.java
+++ splitcheckmathlibrary/src/main/java/com/android/SplitCheckMath.java
@@ -2,6 +2,20 @@ package com.android;
 
 public class SplitCheckMath {
 
+    // Users Class
+    /**
+     * String - First Name
+     * String - Last Name
+     * Int - ID
+     * Boolean - isGoogleLogin
+     * Int - Money Owed
+     */
+
+    // Check Class
+    /**
+     *
+     */
+
     // Split Check
-    
+    // Return - ArrayList of Users
 }
